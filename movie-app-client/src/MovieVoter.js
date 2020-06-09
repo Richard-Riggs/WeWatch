215c46d92ac320484100b4f8d64bbaeec223801f
Francis-Riggs <59446898+Francis-Riggs@users.noreply.github.com>
2020-06-09 14:03:36 -0400

    Move MovieVoter MovieList component into VoteGrid component

--- movie-app-client/src/MovieVoter.js
+++ movie-app-client/src/MovieVoter.js
@@ -1,23 +1,20 @@
 import React, { useContext, useEffect } from 'react';
-import Navbar from './Navbar';
 import MovieVoterNav from './MovieVoterNav';
 import useStyles from './styles/MovieVoterStyles';
 import VotingLobby from './VotingLobby';
 import { VoteSessionContext } from './contexts/VoteSessionContext';
-import MovieList from './MovieList';
 import VoteResults from './VoteResults';
+import VoteGrid from './VoteGrid';
 
 export default function MovieVoter({ history }) {
-  const { stage, error, movieList, voteLimit } = useContext(VoteSessionContext);
+  const { stage, error } = useContext(VoteSessionContext);
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <MovieVoterNav history={history} />
       {stage === 'lobby' && <VotingLobby />}
-      {(stage === 'vote' || stage === 'revote') && (
-        <MovieList mode="vote" movies={movieList.movies} selectLimit={voteLimit} />
-      )}
+      {(stage === 'vote' || stage === 'revote') && <VoteGrid />}
       {stage === 'results' && <VoteResults />}
       {stage === 'error' && <h2>Error: {error}</h2>}
     </div>
