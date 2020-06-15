2ce15fceaf5e6a99a4f9556307d7ec5e5d3fb559
Francis-Riggs <41967519+richy151@users.noreply.github.com>
2020-06-15 14:42:50 -0400

    Make VoteGrid responsive

--- movie-app-client/src/styles/VoteGridStyles.js
+++ movie-app-client/src/styles/VoteGridStyles.js
@@ -1,16 +1,30 @@
 import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles((theme) => ({
-  root : {
-    '& header' : {
-      margin    : '2rem auto',
-      textAlign : 'center',
-      '& h1'    : {
-        marginBottom : '0.75rem',
-        fontSize     : '3rem'
-      },
-      '& p'     : {
-        fontSize : '1.25rem'
-      }
-    }
-  }
+	root: {
+		'& header': {
+			margin: '2rem auto',
+			padding: '0 0.5rem',
+			textAlign: 'center',
+			'& h1': {
+				marginBottom: '0.75rem',
+				fontSize: '3rem'
+			},
+			'& p': {
+				fontSize: '1.25rem'
+			}
+		}
+	},
+	[theme.breakpoints.down('xs')]: {
+		root: {
+			'& header': {
+				margin: '0 0 0.5rem 0',
+				'& h1': {
+					fontSize: '2rem'
+				},
+				'& p': {
+					fontSize: '1rem'
+				}
+			}
+		}
+	}
 }));
