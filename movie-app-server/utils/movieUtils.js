452430a3a809e857d034b8191bee3b756415e304
Francis-Riggs <41967519+richy151@users.noreply.github.com>
2020-06-22 11:12:19 -0400

    Use retry-axios to automatically retry failed API requests

--- movie-app-server/utils/movieUtils.js
+++ movie-app-server/utils/movieUtils.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const rax = require('retry-axios');
+const interceptorId = rax.attach();
 
 // Input query type (discover, search, or trending) and parameters (JSON format)
 // Return array of TMDb movie objects
@@ -7,56 +9,91 @@ exports.getMovies = async (queryType, queryParams) => {
 		? '/' + queryParams.trendTime
 		: ''}`;
 	delete queryParams.trendTime;
-	const movies = await axios.get(url, {
-		params: {
-			api_key: process.env.TMDB_KEY,
-			...queryParams
-		}
-	});
-	return { totalPages: movies.data.total_pages, movies: movies.data.results };
+	try {
+		const movies = await axios.get(url, {
+			params: {
+				api_key: process.env.TMDB_KEY,
+				...queryParams
+			},
+			raxConfig: {
+				retry: 3,
+				noResponseRetries: 3,
+				retryDelay: 100,
+				httpMethodsToRetry: [ 'GET', 'HEAD', 'OPTIONS', 'DELETE', 'PUT' ],
+				statusCodesToRetry: [ [ 100, 199 ], [ 401, 429 ], [ 500, 599 ] ]
+			}
+		});
+		return { totalPages: movies.data.total_pages, movies: movies.data.results };
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 // Input array of TMDb movie objects
 // Return array of TMDb movie objects with IMDb IDs
 exports.addImdbIds = async (movies) => {
-	const updatedMovies = await Promise.all(
-		movies.map(async (movie) => {
-			const response = await axios.get(
-				`https://api.themoviedb.org/3/movie/${movie.id}/external_ids?api_key=${process.env.TMDB_KEY}`
-			);
-			const updatedMovie = { ...movie, imdb_id: response.data.imdb_id };
-			return updatedMovie;
-		})
-	);
-	return updatedMovies;
+	try {
+		const updatedMovies = await Promise.all(
+			movies.map(async (movie) => {
+				const response = await axios.get(
+					`https://api.themoviedb.org/3/movie/${movie.id}/external_ids?api_key=${process.env.TMDB_KEY}`,
+					{
+						raxConfig: {
+							retry: 3,
+							noResponseRetries: 3,
+							retryDelay: 100,
+							httpMethodsToRetry: [ 'GET', 'HEAD', 'OPTIONS', 'DELETE', 'PUT' ],
+							statusCodesToRetry: [ [ 100, 199 ], [ 401, 429 ], [ 500, 599 ] ]
+						}
+					}
+				);
+				const updatedMovie = { ...movie, imdb_id: response.data.imdb_id };
+				return updatedMovie;
+			})
+		);
+		return updatedMovies;
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 // Input array of TMDb movie objects with IMDb IDs
 // Returns array of TMDb movie objects with additional OMDb data
 exports.addOmdbData = async (movies) => {
-	const updatedMovies = await Promise.all(
-		movies.map(async (movie) => {
-			const response = await axios.get('http://www.omdbapi.com/', {
-				params: {
-					apikey: process.env.OMDB_KEY,
-					i: movie.imdb_id
-				}
-			});
-			const { Runtime, Ratings, Genre, Director, Writer, Actors } = response.data;
-			const updatedMovie = {
-				...movie,
-				runtime: Runtime,
-				ratings: Ratings,
-				genre: Genre,
-				director: Director,
-				writer: Writer,
-				actors: Actors,
-				list_id: ''
-			};
-			return updatedMovie;
-		})
-	);
-	return updatedMovies;
+	try {
+		const updatedMovies = await Promise.all(
+			movies.map(async (movie) => {
+				const response = await axios.get('http://www.omdbapi.com/', {
+					params: {
+						apikey: process.env.OMDB_KEY,
+						i: movie.imdb_id
+					},
+					raxConfig: {
+						retry: 3,
+						noResponseRetries: 3,
+						retryDelay: 100,
+						httpMethodsToRetry: [ 'GET', 'HEAD', 'OPTIONS', 'DELETE', 'PUT' ],
+						statusCodesToRetry: [ [ 100, 199 ], [ 401, 429 ], [ 500, 599 ] ]
+					}
+				});
+				const { Runtime, Ratings, Genre, Director, Writer, Actors } = response.data;
+				const updatedMovie = {
+					...movie,
+					runtime: Runtime,
+					ratings: Ratings,
+					genre: Genre,
+					director: Director,
+					writer: Writer,
+					actors: Actors,
+					list_id: ''
+				};
+				return updatedMovie;
+			})
+		);
+		return updatedMovies;
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 // Input array of TMDb movie objects with additional OMDb data
