a95cbfd3373af3f63f99c51318591c53ae50097b
Francis-Riggs <59446898+Francis-Riggs@users.noreply.github.com>
2020-06-30 16:21:51 -0400

    Configure backend to serve app build

--- movie-app-server/index.js
+++ movie-app-server/index.js
@@ -6,12 +6,14 @@ const bodyParser = require('body-parser');
 //===================== SERVER SETUP ====================
 const app = express();
 const http = require('http').createServer(app);
+app.use(express.static('../movie-app-client/build'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //===================== ROUTES ===========================
-app.get('/', (req, res) => res.send('Hello from backend'));
 app.use('/api/movies', require('./routes/movies'));
+app.get('/*', (req, res) => res.sendFile('movie-app-client/build/index.html', { root: '../' }));
+// app.get('*', (req, res) => res.sendFile('../movie-app-client/build/index.html'));
 
 // ================== VOTING SESSIONS ====================
 const io = require('socket.io')(http);
@@ -20,5 +22,5 @@ const voteSockets = vote.sockets(io);
 app.use('/api/vote', vote.router);
 
 //===================== START SERVER ====================
-const port = 3001;
+const port = 3000;
 http.listen(port, () => console.log(`WeWatch server listening on port ${port} `));
