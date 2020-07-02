da9178e1777e00a3b672e28625481fcaa2ed49e3
Francis-Riggs <59446898+Francis-Riggs@users.noreply.github.com>
2020-07-02 15:38:31 -0400

    Update server routes for deployment

--- movie-app-server/index.js
+++ movie-app-server/index.js
@@ -6,13 +6,13 @@ const bodyParser = require('body-parser');
 //===================== SERVER SETUP ====================
 const app = express();
 const http = require('http').createServer(app);
-app.use(express.static('../movie-app-client/build'));
+app.use(express.static('./build'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //===================== ROUTES ===========================
 app.use('/api/movies', require('./routes/movies'));
-app.get('/*', (req, res) => res.sendFile('movie-app-client/build/index.html', { root: '../' }));
+app.get('/*', (req, res) => res.sendFile('build/index.html', { root: '.' }));
 
 // ================== VOTING SESSIONS ====================
 const io = require('socket.io')(http);
@@ -21,5 +21,5 @@ const voteSockets = vote.sockets(io);
 app.use('/api/vote', vote.router);
 
 //===================== START SERVER ====================
-const port = 3000;
+const port = process.env.PORT || 8080;
 http.listen(port, () => console.log(`WeWatch server listening on port ${port} `));
